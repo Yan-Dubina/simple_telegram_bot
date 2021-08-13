ddc03f5ff0c7c0e78ca404ed7afc38d4d9e0d391
Yan Dubina <babyan1997@gmail.com>
2021-08-13 21:33:35 +0200

    bot_config

--- bot/src/main/java/com/telegram_bot/Config/BotConfig.java
+++ bot/src/main/java/com/telegram_bot/Config/BotConfig.java
@@ -1,2 +1,24 @@
-package com.telegram_bot.Config;public class BotConfig {
+package com.telegram_bot.Config;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+import java.math.BigInteger;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@Document(collection = "bot_config")
+
+public class BotConfig {
+    @Id
+    private BigInteger id;
+
+    private String name;
+
+    private String accessToken;
+    
 }
